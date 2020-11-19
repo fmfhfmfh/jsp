f91ac9f44dcb0904c372597cc9e48d7aa4647ae0
dspark <fmfhfmfh@gmail.com>
2020-11-19 09:05:46 +0900

    jackson databind버전수정

--- src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
+++ src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
@@ -31,17 +31,6 @@ public class MemberServiceTest extends ModelTestConfig {
 		assertEquals(1, cnt);
 	}
 	
-////	@Test
-//	public void insertMember_FAIL_Test() {
-//		/***Given***/
-//		MemberVO mv = new MemberVO("ddit", "dditpass", "대덕", "404", "", "", "", "", "");
-//		
-//		/***When***/
-//		int cnt = memberService.insertMember(mv);
-//		
-//		/***Then***/
-//		assertEquals(1, cnt);
-//	}
 	
 	@Test
 	public void getMemberTest() {
